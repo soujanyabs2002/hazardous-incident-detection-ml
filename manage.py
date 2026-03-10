4f1f15427e926b3bdd7705d4ca06e088a12138e2
soujanyabs2002 <soujanyabs2002@gmail.com>
2026-03-10 18:03:54 +0530

    Add files via upload
    Initial commit - Hazardous Incident Detection using ML

---
+++ manage.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Detecting_and_Managing_Railway.settings')  # Update this line to your new settings module
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        )(exc)
+    execute_from_command_line(sys.argv)
+
+if __name__ == '__main__':
+    main()
